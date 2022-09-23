aca55d9931d581006928e05de273dbd9d141c85f
rake3344 <juan.agudelo17@udea.edu.co>
2022-09-23 17:57:51 -0500

    Se creo el paquete controller y se crearon las clases

--- ParqueAndo/src/main/java/controller/IUsuarioController.java
+++ ParqueAndo/src/main/java/controller/IUsuarioController.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package controller;
 
-/**
- *
- * @author usuario
- */
-public class IUsuarioController {
+
+public interface IUsuarioController {
+    public String login(String email_usr, String contrasena_usr);
+        
     
 }
--- ParqueAndo/src/main/java/controller/UsuarioController.java
+++ ParqueAndo/src/main/java/controller/UsuarioController.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package controller;
 
-/**
- *
- * @author usuario
- */
-public class UsuarioController {
+package controller;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import com.google.gson.Gson;
+import Beans.Usuario;
+import connection.DBConnection;
+
+public class UsuarioController implements IUsuarioController {
+    
+    @Override
+    public String login(String email_usr, String contrasena_usr){
+        Gson gson = new Gson();
+        DBConnection con = new DBConnection();
+        String sql = "SELECT * FROM usuario WHERE email_usr = '" + email_usr + "' and contrasena_usr = '" + contrasena_usr + "'";
+        
+        try {
+            Statement st = con.getConnection().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                double cel_usr = rs.getDouble("cel_usr");
+                String placa = rs.getString("placa");
+                String marca = rs.getString("marca");
+                String color = rs.getString("color");
+                double dinero_disp = rs.getDouble("dinero_disp");
+                boolean estado_usr = rs.getBoolean("estado_usr");
+                
+                boolean premium = rs.getBoolean("premium");
+                Usuario usuario;
+                usuario = new Usuario(email_usr, contrasena_usr, nombre, apellido, cel_usr, placa, marca, color,dinero_disp, estado_usr, id);
+                return gson.toJson(usuario);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            con.desconectar();
+        }
+        return "false";
+    }
     
 }
