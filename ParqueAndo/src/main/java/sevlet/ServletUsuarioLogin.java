2fdb1043fbc23d7fd6a3fb48e1e250a3c170f07f
rake3344 <juan.agudelo17@udea.edu.co>
2022-09-23 17:58:45 -0500

    Se creo ServletUsuarioLogin

---
+++ ParqueAndo/src/main/java/sevlet/ServletUsuarioLogin.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sevlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.UsuarioController;
+
+/**
+ * Servlet implementation class ServletUsuarioLogin
+ */
+@WebServlet("/ServletUsuarioLogin")
+public class ServletUsuarioLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletUsuarioLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+        @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		UsuarioController usuario = new UsuarioController();
+		String email_usr = request.getParameter("email_usr");
+		String contrasena_usr = request.getParameter("contrasena_usr");
+		String result = usuario.login(email_usr, contrasena_usr);
+		
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println(result);
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+        @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
