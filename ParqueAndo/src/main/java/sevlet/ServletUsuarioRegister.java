80429fa6818783c1459b9ad1878cfe744cf2bfd9
rake3344 <juan.agudelo17@udea.edu.co>
2022-09-26 19:35:29 -0500

    ServletUsuarioRegister

---
+++ ParqueAndo/src/main/java/sevlet/ServletUsuarioRegister.java
@@ -0,0 +1,71 @@
+package sevlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.UsuarioController;
+
+/**
+ * Servlet implementation class ServletUsuarioRegister
+ */
+@WebServlet("/ServletUsuarioRegister")
+public class ServletUsuarioRegister extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletUsuarioRegister() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        UsuarioController usuario = new UsuarioController();
+        
+        int id = Integer.parseInt(request.getParameter("id"));
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        double cel_usr = Double.parseDouble(request.getParameter("cel_usr"));      
+        String email_usr = request.getParameter("email_usr");
+        String placa = request.getParameter("placa");
+        String marca = request.getParameter("marca");
+        String color = request.getParameter("color");
+        double dinero_disp = Double.parseDouble(request.getParameter("dinero_disp"));
+        String contrasena_usr = request.getParameter("contrasena_usr");
+        boolean estado_usr = Boolean.parseBoolean(request.getParameter("estado_usr"));
+        
+
+        String result = usuario.register(id, nombre, apellido, cel_usr, email_usr, placa, marca, color, dinero_disp, contrasena_usr, estado_usr);
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println(result);
+        out.flush();
+        out.close();
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
